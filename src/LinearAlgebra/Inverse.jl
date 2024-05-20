c3b1b0fb4604f8a9cdbbb459c4fec68ce1c2bdbf
Júlia <163569094+juliaboaventuraf@users.noreply.github.com>
2024-05-20 10:00:58 -0300

    Update Inverse.jl

--- src/LinearAlgebra/Inverse.jl
+++ src/LinearAlgebra/Inverse.jl
@@ -1,11 +1,18 @@
 using LinearAlgebra: I
 
 function inverse_matrix(A::Matrix{Float64})
-    n = size(A)[1]
-    
+    if size(A)[1] != size(A)[2]
+        println("A matriz precisa ser quadrada.")
+        return
+    end
+
+    n = size(A)[1]    
     B = Matrix{Float64}(undef, n, n)
     for j = 1:n
         b = 1.0 * I[1:n,j]
+        if isnothing(solve_system_with_scaled_partial_pivoting(A, b))
+            return
+        end
         B[:,j] = solve_system_with_scaled_partial_pivoting(A, b)
     end
     return B
